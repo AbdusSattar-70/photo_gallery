0c3a8a466f9d86e4fc7170f9e8869f1524967ecf
Abdus Sattar <sasorganicagro@gmail.com>
2023-11-01 21:08:38 +0600

    Add google authentication route in the backend

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { signup, signin } = require('../controllers/authController');
+const { signup, signin, google } = require('../controllers/authController');
 const { addUserValidators, addUserValidationHandler } = require('../middlewires/users/userValidators');
 
 const router = express.Router();
+router.post('/google', google);
 router.post('/liveValidate', addUserValidators, addUserValidationHandler);
 router.post('/signup', addUserValidators, addUserValidationHandler, signup);
 router.post('/signin', signin);
