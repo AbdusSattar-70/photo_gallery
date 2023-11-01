eec677d8209138a713d0d4178242172034949618
Abdus Sattar <sasorganicagro@gmail.com>
2023-11-01 15:02:27 +0600

    Add rootReducer

---
+++ frontEnd/src/redux/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from '@reduxjs/toolkit';
+import authSlice from './authSlice';
+
+const rootReducer = combineReducers({
+  auth: authSlice,
+
+});
+
+export default rootReducer;
