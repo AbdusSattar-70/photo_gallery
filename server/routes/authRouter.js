305b399c4abab9d04fdbad7ab849f7e296ef9ce5
Abdus Sattar <sasorganicagro@gmail.com>
2023-11-03 19:23:53 +0600

    Backend: Add gallery route and manage their logic, add gallery model

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { signup, signin, google } = require('../controllers/authController');
+const {
+  signup, signin, google, signOut,
+} = require('../controllers/authController');
 const { addUserValidators, addUserValidationHandler } = require('../middlewires/users/userValidators');
 
 const router = express.Router();
@@ -7,5 +9,6 @@ router.post('/google', google);
 router.post('/liveValidate', addUserValidators, addUserValidationHandler);
 router.post('/signup', addUserValidators, addUserValidationHandler, signup);
 router.post('/signin', signin);
+router.get('/signout', signOut);
 
 module.exports = router;
