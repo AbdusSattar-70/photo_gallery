d02e60d5e55af687cc7d145b69d02ca4e419352c
Abdus Sattar <sasorganicagro@gmail.com>
2023-11-06 14:17:13 +0600

    Fix render error

--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -1,13 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui'
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-     "./node_modules/tw-elements-react/dist/js/**/*.js",
+    "./node_modules/tw-elements-react/dist/js/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [daisyui,"tw-elements-react/dist/plugin.cjs"],
+  plugins: [daisyui, {
+    resolve: "tw-elements-react/dist/plugin.cjs"
+  }],
 }
